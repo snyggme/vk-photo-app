cd3f9cf15aee7d7ccaef0bb782a1c861071ea6e4
snyggme <rcbabahin@gmail.com>
2018-10-31 21:41:43 +0300

    Updates

---
+++ precache-manifest.9f1c25c4820de557a3612f123e9ca12f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f29c8e206ff08904735",
+    "url": "/vk-photo-app/static/js/runtime~main.0f29c8e2.js"
+  },
+  {
+    "revision": "f96e852951a58104aa2c",
+    "url": "/vk-photo-app/static/js/main.f96e8529.chunk.js"
+  },
+  {
+    "revision": "82de3cd697d7c7a97b1c",
+    "url": "/vk-photo-app/static/js/1.82de3cd6.chunk.js"
+  },
+  {
+    "revision": "f96e852951a58104aa2c",
+    "url": "/vk-photo-app/static/css/main.8d079709.chunk.css"
+  },
+  {
+    "revision": "f43dc5f5de1dffed8baa5e4052c01a4b",
+    "url": "/vk-photo-app/index.html"
+  }
+];
